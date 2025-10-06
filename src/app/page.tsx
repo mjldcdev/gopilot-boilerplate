8c1370ea92ad6560bc9fa3faeabaeac210f5950c
Mark Joel Dela Cruz <mjldelacruz.dev@gmail.com>
2025-10-06 12:29:36 +0800

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
 import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Input } from '@/components/ui/input'
+import { Progress } from '@/components/ui/progress'
+import { Textarea } from '@/components/ui/textarea'
 
 export default function Home() {
   return(
-    <div className='flex flex-col gap-2 max-w-max p-2'>
-      <h1 className='text-rose-500'>
-        Hello World!
-      </h1>
-      <Button variant='destructive'>
-        Hello World!
-      </Button>
+    <div className='flex flex-col gap-4 max-w-max p-4'>
+      <div>
+        <Button>
+          I am a `Button`
+        </Button>
+      </div>
+      <div>
+        <Input placeholder='I am an `Input`' />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea defaultValue='I am a `Textarea`' />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   )
 }
